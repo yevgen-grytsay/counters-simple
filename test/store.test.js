22b935d80c9fe0bfe3de7ba7fedb914af9ddb45b
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-03-12 20:12:28 +0200

    add tests for "curValueFixed" function

--- test/store.test.js
+++ test/store.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { diffSumNumber, diffSum } from '../src/store.js'
+import { diffSumNumber, diffSum, curValueFixed } from '../src/store.js'
 
 test('test all meters total diff number', () => {
     expect(diffSumNumber(['hotWater-kitchen', 'hotWater-bathroom'])).toBeCloseTo(0.2, 1)
@@ -13,4 +13,12 @@ test('test all meters total diff fixed', () => {
     expect(diffSum(['coldWater-kitchen', 'coldWater-bathroom'])).toBe('1.2')
     expect(diffSum(['electricity-day'])).toBe('33.0')
     expect(diffSum(['electricity-night'])).toBe('8.3')
+})
+
+
+test('curValueFixed', () => {
+    expect(curValueFixed('hotWater-kitchen')).toBe('39.1')
+    expect(curValueFixed('hotWater-bathroom')).toBe('41.3')
+    expect(curValueFixed('coldWater-kitchen')).toBe('39.1')
+    expect(curValueFixed('coldWater-bathroom')).toBe('279.8')
 })
